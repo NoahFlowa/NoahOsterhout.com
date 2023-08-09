6a3c8c75fb02cb3da25bf7b108ca07ae76cb1c07
Noah Osterhout <osterhoutn@gmail.com>
2023-08-09 15:08:16 -0400

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" style="color: #22c2fc;" href="https://github.com/NoahFlowa/Blog"> blog</a>
+                    <a class="nav-link" style="color: #22c2fc;" href="https://blog.noahosterhout.com"> blog</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" style="color: #22c2fc;" href="https://github.com/NoahFlowa"> github</a>

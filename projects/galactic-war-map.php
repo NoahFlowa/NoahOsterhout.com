800fef39eb65d45a76b8eaf0d9ac26aafa168141
NoahFlowa <osterhoutn@gmail.com>
2025-01-08 16:17:17 -0500

    Added Galactic War Map project

---
+++ projects/galactic-war-map.php
@@ -0,0 +1,19 @@
+<div class="project-container mt-3">
+    <div class="project-header">
+        <span class="back-button fs-2">
+            <i class="fas fa-arrow-left"></i>
+        </span>
+        <h2 class="fs-2 mb-0">Galactic War Map (GWM)</h2>
+    </div>
+    
+    <div class="project-section">
+        <h3 class="fs-3 mb-3">Overview</h3>
+        <p>The Galactic War Map is a 3D map that shows the current lines of the Second Galactic War in Helldivers 2.  The map is a crucial interface in the game which allows the players to see the current state of the war, the current missions available, the current rewards available, and the current state of the war.</p>
+    </div>
+    
+    <div class="project-section">
+        <h3 class="fs-3 mb-3">Case Study</h3>
+        <p class="mt-3">This project is still a WIP.  As I continue making the app, I will write more about it here.</p>
+    </div>
+
+</div>
